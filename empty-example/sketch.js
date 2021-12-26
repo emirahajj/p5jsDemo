b33788fdcc2d6fe588cc7e021357227167639133
emira <emirahajj@gmail.com>
2021-12-25 23:59:14 -0500

    pruning extra code

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,24 +1,15 @@
-//setup runs once
-let newX = Math.PI;
-let translateX = 10;
-let translateY = 0;
 let angles = [];
-let angleV = 0.0082;
 
 let palette = [
-  [55, 57, 58 ],
+  [30, 30, 30],
   [119, 182, 234],
   [203, 156, 242],
   [158, 123, 155]
 ]; //stores 3 RBG colors
 
-//let particles = []; //array that will store our particles
-let particleRings = []; //array that will store our particles
 let particleRing1, particleRing2, particleRing3, particleRing4, particleRing5;
 let particleWave;
 
-let vector; //vector to determine which direction to move the individual particles in
-let offset = 40;
 let time = 0;
 
 // Constants
@@ -49,73 +40,46 @@ function setGradient(x, y, w, h, c1, c2, axis) {
   }
 }
 
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  particleRing4 = new ParticleRing(0, 0, 175, 7, palette[1], 1);
-  particleRing5 = new ParticleRing(0, 0, 175, 7, palette[1], 1);
+  particleRing4 = new ParticleRing(0, 0, 100, 7, palette[1], 1);
+  particleRing5 = new ParticleRing(0, 0, 400, 7, palette[2], 1);
 
   particleRing1 = new ParticleRing(0, 0, 400, 7, palette[2], 1);
   particleRing2 = new ParticleRing(0, 0, 0, 30, palette[1], 1 / 4);
   particleRing3 = new ParticleRing(0, 0, 160, 30, palette[3], 1 / 2);
-  //particleWave = new ParticleWave(0, 0, 10, 10, palette[1], 4)
-  b1 = color(255);
-  b2 = color(0);
+
+  b1 = color(palette[0]);
+  b2 = color(palette[1]);
 
   stroke(0);
-
-    // Define colors
-
 }
 
 //draw forever
 function draw() {
   angleMode(RADIANS);
   rectMode(CENTER);
-  //translateX+=10;
   background(palette[0]);
-  //setGradient(0, 0, windowWidth, windowHeight, b1, b2, X_AXIS);
-  //setGradient(windowWidth / 2, 0, windowWidth / 2, windowHeight, b2, b1, X_AXIS);
 
-  //line(windowWidth / 2, 0, windowWidth / 2, windowHeight);
-  //line(0, windowHeight / 2, windowWidth, windowHeight / 2);
+  line(windowWidth / 2, 0, windowWidth / 2, windowHeight);
+  line(0, windowHeight / 2, windowWidth, windowHeight / 2);
   translate(windowWidth / 2, windowHeight / 2)
 
-  // if (time < 2 * PI) {
-  // } else if (time >= 2* PI){
-  //   time -= PI/40;
-  // }
-
-  time += PI / 200;
-  //console.log(time);
-    // Background
-
-
+  time+= PI/40;
+  rotate(time/100)
 
   //you can call any new type of 
   let v = createVector(200 * sin(millis() / 300), 200 * cos(millis() / 300));
   //number multiplied outside trig func makes it longer
-  let vv = createVector((100 * (sin(time/1.5))), (100 * cos(time/1.5)));
+  let vv = createVector((50 * (sin(time))), (50 * cos(time)));
   //console.log(v);
   let v1 = createVector(50 * time, sin(50 * time));
-  let v2 = p5.Vector.fromAngle(time * 10, 70);
+  let v2 = p5.Vector.fromAngle(time, 5);
   // let v2 = p5.Vector.fromAngle(time * 8, 50);
 
-  
-  
-
-  // if(particleRing4.reachedEnd()){
-  //   particleRing4.show(1, v2);
-  // }
-  particleRing4.show(1, v2);
-  //particleRing5.show(1, v2);
-  particleRing1.show(1, v2);
-  //particleRing2.show(1, v2);
-
-  // particleRing3.show(1, v);
-  // particleRing2.show(1, v);
-  //particleRing1.show(-1, v);
+  particleRing4.show(0.4, vv);
+  particleRing5.show(0.4, vv);
 
 }
 
@@ -142,14 +106,6 @@ class Particle {
     this.history.push(v);
 
 
-    //experiment with drawingCOntect
-    // drawingContext.shadowOffset = 5;
-    // drawingContext.shadowOffsetX = 5;
-    // drawingContext.shadowOffsetY = 5;
-    // drawingContext.shadowBlur = 30;
-    // drawingContext.shadowColor = 'black';
-    //drawingContext.globalCompositeOperation = "source-over";
-
     // beginShape();
 
     for (var i = 0; i < this.history.length; i++) {
@@ -159,27 +115,17 @@ class Particle {
       }
       strokeCap(ROUND);
       strokeJoin(ROUND);
-      strokeWeight(this.size/2);
+      strokeWeight(map(i, 0, this.history.length, 0, this.size));
       stroke(this.color[0], this.color[1], this.color[2], map(i, 0, this.history.length, 0, 255));
-      // stroke(this.color[0], this.color[1], this.color[2],255);
+      stroke(this.color[0], this.color[1], this.color[2],255);
       noFill();
-      //vertex(pos.x, pos.y);
+      // vertex(pos.x, pos.y);
       point(pos.x, pos.y)
     }
     // endShape();
 
     pop();
   }
-
-  reachedEnd(){
-    if(this.history.length <2){
-      if (this.history[this.history.length-1].x == this.x && this.history[this.history.length-1].y == this.y){
-        return true;
-      }
-    }
-    return false;
-  }
-
 }
 
 
@@ -207,44 +153,19 @@ class ParticleRing {
     }
   }
 
-  reachedEnd(){
-      for (var i = 0; i < this.particles.length; i++) {
-        if(this.particles[i].reachedEnd()){
-          return true;
-        }
-      }
-    return false;
-  }
-
   //this is a render method
   show(direction, v) {
     push();
     noStroke();
-    translate(this.x, this.y);
-    //translate(v);
-    //translate(pathFunc.x, pathFunc.y);
+    //translate(this.x/100, this.y/100);
     for (let i = 0; i < this.particles.length; i++) {
-      // this.particles[i].x = v.x;
-      // this.particles[i].y = v.y;
       this.particles[i].vector = v;
       let prevX = this.particles[i].x;
       let prevY = this.particles[i].y;
-      let rotatedX = prevX * cos(newX * direction / 60) - prevY * sin(newX * direction / 60);
-      let rotatedY = prevX * sin(newX * direction / 60) + prevY * cos(newX * direction / 60);
-      //console.log(rotatedX, rotatedY)
+      let rotatedX = prevX * cos(PI * direction / 100) - prevY * sin(PI * direction / 100);
+      let rotatedY = prevX * sin(PI * direction / 100) + prevY * cos(PI * direction / 100);
       this.particles[i].x = rotatedX
       this.particles[i].y = rotatedY
-      //console.log(this.particles[i].x, this.particles[i].y);
-      this.particles[i].show();
-    }
-    //translate(-this.x, -this.y)
-    pop();
-  }
-
-  renderRing() {
-    push();
-    noStroke();
-    for (let i = 0; i < this.particles.length; i++) {
       this.particles[i].show();
     }
     pop();
@@ -284,7 +205,7 @@ class ParticleWave {
       //strokeWeight(1);
       let x = map(i, 0, angles.length, -windowWidth / 2, windowWidth / 2)
       //circle(x, y, this.particleSize);
-      angles[i] += angleV;
+      angles[i] += 0.0082;
       vertex(x, y);
     }
     endShape();
